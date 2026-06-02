5400f0494321a9b31da26394fe02790446ea5cbd
BaeEu <sabaloo20111990@gmail.com>
2026-06-02 19:22:20 +0630

    check db

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-  //   apiUrl: 'http://localhost:5000/api'
+  // apiUrl: 'http://localhost:5000/api'
   apiUrl: 'https://ai-interview-assistant-mxh2.onrender.com/api'
 };
