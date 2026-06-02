29c95f5342443dd396710ae757fed659b7e36c4a
BaeEu <sabaloo20111990@gmail.com>
2026-06-02 19:06:55 +0630

    Change URL

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 console.log(
+  process.env.DATABASE_URL?.substring(0, 30)
+);
+
+console.log(
   "DATABASE_URL FOUND:",
   !!process.env.DATABASE_URL
 );
